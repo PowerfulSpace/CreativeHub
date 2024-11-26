29b0ff82e6265de941d379f3ec749d9672a30e4e
Alexander <sany199574@gmail.com>
2024-11-26 07:17:52 +0300

    Добавлена сущность   - ContactFormEntry

---
+++ src/CreativeHub/PS.CreativeHub.Web/Models/ContactFormEntry.cs
@@ -0,0 +1,12 @@
+﻿namespace PS.CreativeHub.Web.Models
+{
+    public class ContactFormEntry
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Zodiac { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Weapon { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}
